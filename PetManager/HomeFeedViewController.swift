87354de6c3a0a85fb0878212e17b44bf959ef2d5
Yihui Liao <liao.yihui18@gmail.com>
2020-11-23 14:47:53 -0500

    user can sign in

---
+++ PetManager/HomeFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeFeedViewController.swift
+//  PetManager
+//
+//  Created by Yihui Liao on 11/23/20.
+//
+
+import UIKit
+
+class HomeFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
