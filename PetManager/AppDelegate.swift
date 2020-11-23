2f56cfe761c9b350dcb7e38482a6ef065e7415d5
Yihui Liao <liao.yihui18@gmail.com>
2020-11-23 13:58:42 -0500

    connected app to parse backend

--- PetManager/AppDelegate.swift
+++ PetManager/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "k0ymzzCbwu7Uh0YONLfiyYJX40HB7KWoMGdVvSwV"
+                    $0.clientKey = "tLkfUYKZmaP2ZLzTSbnWEFa5degVNzMw0Kl82DNX" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
