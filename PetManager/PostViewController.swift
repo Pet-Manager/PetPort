7fac831fd0b0022cab034ac9c67cc48a4d128ef5
Diane Nguyen <diane.nguyen.124@gmail.com>
2020-11-28 20:54:30 -0800

    Basic post functionality working, can keep track if post is health or not

--- PetManager/PostViewController.swift
+++ PetManager/PostViewController.swift
@@ -6,9 +6,62 @@
 //
 
 import UIKit
+import Parse
 
 class PostViewController: UIViewController {
+    
+    //pet dummy data
+    func petData() -> PFObject {
+        let pet = PFObject(className: "Pets")
+        pet["name"] = "Douglas"
+        pet["author"] = PFUser.current()!
+        
+        return pet
+    }
 
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var uncheckedButton: UIButton!
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = textView.text!
+        post["isHealth"] = isHealth
+        post["author"] = PFUser.current()!
+        post["pet"] = petData() // after add pet, figure out how to select pet
+        
+        print("health status: \(isHealth)")
+        
+        post.saveInBackground{ (success, error) in
+            if success {
+                print("saved post")
+                // then switch tab control to home feed view
+            } else {
+                print("error saving post")
+            }
+            
+        }
+        
+    }
+    
+    var unchecked:Bool = true
+    var isHealth:Bool = false
+    
+    @IBAction func checkedBox(_ sender: Any) {
+        if (unchecked) {
+            uncheckedButton.setImage(UIImage(named:"checked"), for: UIControl.State.normal)
+            unchecked = false
+            isHealth = true
+            print("checked")
+        } else {
+            uncheckedButton.setImage(UIImage(named:"unchecked"), for: UIControl.State.normal)
+            unchecked = true
+            isHealth = false
+            print("unchecked")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
