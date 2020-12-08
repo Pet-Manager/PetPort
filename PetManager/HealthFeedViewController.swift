b7e36da908a3b9659f94d4f9d671aad92c1f5ab0
Yihui Liao <liao.yihui18@gmail.com>
2020-12-07 20:31:19 -0500

    updated health feed with collection view (not complete)

--- PetManager/HealthFeedViewController.swift
+++ PetManager/HealthFeedViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import Parse
 
-class HealthFeedViewController: UIViewController {
+class HealthFeedViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var pets = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        self.collectionView.reloadData()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let user = PFUser.current()
+        self.pets = user?["pets"] as! [PFObject]
+        self.collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pets.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PetCollectionViewCell", for: indexPath) as! PetCollectionViewCell
+        
+        return cell
+    }
+    
     
 
     /*
