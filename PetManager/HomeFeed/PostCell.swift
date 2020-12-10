2787dddada4cd8560f14dea34f5859831c849e24
Diane Nguyen <diane.nguyen.124@gmail.com>
2020-12-10 13:38:52 -0800

    Home feed displays posts with and without image

---
+++ PetManager/HomeFeed/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  PetManager
+//
+//  Created by Diane Nguyen on 12/9/20.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var petNameLabel: UILabel!
+    @IBOutlet weak var petProfileView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var createdAtLabel: UILabel!
+    @IBOutlet weak var userLabel: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
